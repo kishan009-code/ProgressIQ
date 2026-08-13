1994ca5861f03dd2281e4a8cfd59002660f87436
kishan009-code <kishansahil5@gmail.com>
2026-08-13 21:04:37 +0530

    Add login page and Flask form handling

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("Home.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        email = request.form["email"]
+        password = request.form["password"]
+
+        if email == "test@gmail.com" and password == "12345":
+            return "Login Successful!"
+
+        else:
+            return "Invalid email or password"
+
+    return render_template("Login.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
